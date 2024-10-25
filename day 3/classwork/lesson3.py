0f2655ed71fabe774bbcbb4eb0d7ee23e3bf9a35
giorgigagua <giorgigagua18@gmail.com>
2024-10-25 22:30:36 +0400

    commit

---
+++ day 3/classwork/lesson3.py
@@ -0,0 +1,5 @@
+name = "Giorgi"
+lastname = "Gagua"
+age = "12"
+favoriteanimal = "dog"
+
---
+++ day 3/homework/lesson3.py
@@ -0,0 +1,10 @@
+name="giorgi"
+lastname="gagua"
+age="12"
+hobby="football"
+city="tbilisi"
+print(name)
+print(lastname)
+print(age)
+print(hobby)
+print(city)
